a914b9d799d736c83f00f02087f7c2ce29bec169
Joshh <j-hagedorn@users.noreply.github.com>
2020-12-13 17:59:00 -0500

    Update fetch_taletypes.R

--- fetch/fetch_taletypes.R
+++ fetch/fetch_taletypes.R
@@ -2,8 +2,16 @@
 df <-
   read_lines("fetch/ATU.Master.Hels.txt") %>%
   as_tibble() %>%
-  # Remove front matter and index
+  rename(text = value) %>%
+   # Remove front matter and index
   slice(173:16129) %>%
   mutate(
-    id = str_extract(value,"([0-9]+).*$")
+    type = case_when(
+      str_detect(text,"^\\s$")                ~ NA_character_,
+      str_detect(text,"Combinations:")        ~ "combos",
+      str_detect(text,"Remarks:")             ~ "remarks",
+      str_detect(text,"Literature/Variants:") ~ "litvar",
+      str_detect(text,"^[A-Z|\\s].*[0-9]$")   ~ "section",
+      str_detect(text,"^[A-Z|\\s]+$")         ~ "heading"
+    )
   )
