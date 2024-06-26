c2feaa514008c0299abaceaae13c954b3f1fdff8
Joshh <j-hagedorn@users.noreply.github.com>
2024-06-25 23:40:41 -0400

    Update fetch_gutenberg.R

--- fetch_gutenberg.R
+++ fetch_gutenberg.R
@@ -13,7 +13,10 @@ x <-
     id = str_remove(id,"/pg$")
   )
 
-df <-
+# Folklore bookshelf
+# https://www.gutenberg.org/ebooks/bookshelf/37
+
+ref <-
   gutenberg_works(gutenberg_id %in% x$id) %>%
   distinct(gutenberg_id,.keep_all = T) %>%
   mutate(
@@ -23,5 +26,45 @@ df <-
     )
   )
 
-# Folklore bookshelf
-# https://www.gutenberg.org/ebooks/bookshelf/37
+# Read in texts
+
+lookup <-
+  tribble(
+    ~gutenberg_id, ~start_toc, ~stop_toc, ~start_text, ~stop_text,
+    ############# | ######### | ######## | ########## | #########
+    "4018",        14,         35,        98,          7548,
+    "2198",        23,         54,        61,          7188
+  )
+
+i <- 2
+
+df <- gutenberg_download(lookup$gutenberg_id[i])
+
+toc <- 
+  df %>% 
+  slice(lookup$start_toc[i]:lookup$stop_toc[i]) %>% 
+  mutate(
+    text = str_squish(text),
+    text = str_remove(text,"[0-9]+\\."),
+    text = str_extract(text, "[^\\.]+"),
+    text = str_squish(text)
+  ) %>%
+  .$text
+
+y <- 
+  df %>%
+  mutate(
+    text = str_squish(text),
+    tale = if_else(
+      str_detect(text,regex(paste(toc,collapse = "|"),ignore_case = T)),
+      text,NA_character_
+    )
+  ) %>%
+  fill(tale) %>% 
+  slice(lookup$start_text[i]:lookup$stop_text[i]) %>%
+  filter(text != "") %>%
+  filter(text != tale) %>%
+  group_by(gutenberg_id,tale) %>%
+  summarize(text = paste(text,collapse = " ")) %>%
+  ungroup()
+  
